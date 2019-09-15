0a7f871d4c75dd29e8753aaaaf27963233958538
Christophe Van Neste <christophe.vanneste@kaust.edu.sa>
2019-09-15 17:00:48 +0300

    check duplicates in pubmed results

--- sina/documents/__init__.py
+++ sina/documents/__init__.py
@@ -496,6 +496,11 @@ class PubmedQueryResult(object):
     def __init__(self, corpus, results, test_fraction=.25):
         import pandas as pd
         self.results = pd.DataFrame(results).set_index('pmid')
+        duplicates = self.results.index.duplicated().sum()
+        if duplicates:
+            import warnings
+            warnings.warn('Removing %s pmid duplicates' % 100)
+            self.results = self.results.reset_index().drop_duplicates('pmid').set_index('pmid')
         self.corpus = corpus
         if isinstance(test_fraction, pd.DataFrame):
             self.results_test = test_fraction
