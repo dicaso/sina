bfe20e009008be098c434508a1dc392363a4804f
Christophe Van Neste <christophe.vanneste@kaust.edu.sa>
2019-09-25 08:25:54 +0300

    included loggging

--- sina/paperwork/pancancer.py
+++ sina/paperwork/pancancer.py
@@ -2,17 +2,12 @@
 ## Imports
 from sina.documents import PubmedCollection, PubmedQueryResult
 from collections import OrderedDict
-import numpy as np, pandas as pd, os, time, zipfile, argparse
+import numpy as np, pandas as pd
+import logging, os, time, zipfile, argparse
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['figure.max_open_warning'] = 50
 #plt.ion()
-
-saveloc = os.path.expanduser('~/Projects/topicmining/')+time.strftime("%Y%m%d-%H%M%S")
-os.mkdir(saveloc)
-def savefig(fig,filename):
-    fig.savefig(os.path.join(saveloc,filename+'.png'))
-    fig.savefig(os.path.join(saveloc,filename+'.pdf'))
 
 # Argparse for ML/NN settings
 parser = argparse.ArgumentParser()
@@ -21,8 +16,27 @@ parser.add_argument('--w2vecsize', default = 100, type=int)
 parser.add_argument('--k_clusters', default = 100, type=int)
 parser.add_argument('--topmesh', default = 10, type=int)
 parser.add_argument('--getmeshnames', default=False, nargs='?', const=True)
+parser.add_argument('--outputdir')
 settings = parser.parse_args()
 
+saveloc = settings.outputdir | os.path.expanduser(
+    '~/Projects/topicmining/')+time.strftime("%Y%m%d-%H%M%S")
+os.mkdir(saveloc)
+def savefig(fig,filename):
+    fig.savefig(os.path.join(saveloc,filename+'.png'))
+    fig.savefig(os.path.join(saveloc,filename+'.pdf'))
+
+# Logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    #filename=os.path.join(saveloc,'sina.log'), filemode='w',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(os.path.join(saveloc,'sina.log'))
+    ],
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+    
 ## cancertypes
 ## https://www.cancer.gov/about-nci/organization/ccg/research/structural-genomics/tcga/studied-cancers
 cancertypes = OrderedDict((
@@ -65,6 +79,7 @@ cancertypes = OrderedDict((
 ))
 
 # Load corpora
+logging.info('Loading pubmed collection')
 pmc = PubmedCollection('pubmed','~/pubmed')
 corpora = {
     ct: PubmedQueryResult(results=pmc.query_document_index(ct),corpus=pmc)
@@ -73,6 +88,7 @@ corpora = {
 
 # Generic embeddings
 ## Create PubmedQueryResult that merges all specific PubmedQueryResults
+logging.info('preparing allcancers embedding')
 allcancers_training = pd.concat(
         [corpora[ct].results for ct in cancertypes],
         sort=False
@@ -106,6 +122,7 @@ else:
     allcancers.nn_keras_predictor(textprep=False)
 
 ## GloVe
+logging.info('preparing glove embedding')
 ### download wiki trained glove
 glovepth = os.path.expanduser('~/tmp/glove.6B.zip')
 if not os.path.exists(glovepth):
@@ -121,9 +138,9 @@ glove2word2vec.glove2word2vec(glovepth[:-3]+'100d.txt',glovepth[:-3]+'100d.w2v.t
 glovemdl = gensim.models.KeyedVectors.load_word2vec_format(glovepth[:-3]+'100d.w2v.txt')
     
 for ct in cancertypes:
-    print(ct)
+    logging.info(ct)
     corpora[ct].transform_text(preprocess=True,method='tfid')
-    corpora[ct].analyze_mesh(topfreqs=topmesh,getmeshnames=settings.getmeshnames) #TODO change code to execute on workstation
+    corpora[ct].analyze_mesh(topfreqs=settings.topmesh,getmeshnames=settings.getmeshnames) #TODO change code to execute on workstation
     corpora[ct].gensim_w2v(vecsize=settings.w2vecsize)
     #corpora[ct].k_means_embedding(k=settings.k_clusters)
     corpora[ct].predict_meshterms(model='svm', kmeans_only_freqs=False, rebalance='oversample')
@@ -142,3 +159,66 @@ fig, ax = plt.subplots()
 plt.imshow(docoverlap, cmap='viridis')
 plt.colorbar()
 savefig(fig,'corpora_overlap')
+
+customs = []
+generics = []
+for ct in cancertypes:
+    corpus = corpora[ct]
+    print(
+        ct,
+        len(corpus.results),
+        'generic embedding' if corpus.nn_grid_result.best_params_['embedding'] is allcancers.embedding 
+        else 'custom embedding'
+    )
+    if corpus.nn_grid_result.best_params_['embedding'] is allcancers.embedding:
+        generics.append(ct)
+    else:
+        customs.append(ct)
+
+# Corpus size category
+fig, ax = plt.subplots()
+ax.scatter([len(corpora[ct].results) for ct in generics], [0]*len(generics), label='generic')
+ax.scatter([len(corpora[ct].results) for ct in customs], [1]*len(customs), label='custom')
+ax.legend()
+ax.set_title('Corpus size and the relevance of a custom embedding')
+ax.set_xlabel('Corpus size (#)')
+savefig(fig,'corpus_size')
+## optional look at embedding_trainable
+
+from scipy import stats
+cscores = [corpora[ct].nn_grid_result.best_score_ for ct in customs]
+gscores = [corpora[ct].nn_grid_result.best_score_ for ct in generics]
+print(
+    'generic mean+var', np.mean(gscores), np.var(gscores),
+    '\ncustom mean+var', np.mean(cscores), np.var(cscores),
+    '\nttest', stats.ttest_ind(cscores, gscores)    
+)
+
+# Maximum overlap with other corpus
+fig, ax = plt.subplots()
+ax.scatter([len(corpora[ct].results) for ct in generics], np.where(docoverlap==1, 0, docoverlap).max(axis=1)[[ct in generics for ct in cancertypes]], label='generic')
+ax.scatter([len(corpora[ct].results) for ct in customs], np.where(docoverlap==1, 0, docoverlap).max(axis=1)[[ct in customs for ct in cancertypes]], label='custom')
+ax.legend()
+ax.set_title('Corpus size and maximum overlap with other corpora')
+ax.set_xlabel('Corpus size (#)')
+ax.set_ylabel('Maximum overlap with other corpus (%)')
+
+#3d
+# from mpl_toolkits.mplot3d import Axes3D
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter([len(corpora[ct].results) for ct in generics], np.where(docoverlap==1, 0, docoverlap).max(axis=1)[[ct in generics for ct in cancertypes]], gscores, marker='o', label='generic')
+# ax.scatter([len(corpora[ct].results) for ct in customs], np.where(docoverlap==1, 0, docoverlap).max(axis=1)[[ct in customs for ct in cancertypes]], cscores, marker='^', label='custom')
+# ax.set_xlabel('Corpus size (#)')
+# ax.set_ylabel('Maximum overlap with other corpus (%)')
+# ax.set_zlabel('NN score')
+
+#Maximum overlap with other corpus (%) - marker sized corpus
+from scipy.interpolate import interp1d
+sizemap = interp1d([min([len(corpora[ct].results) for ct in cancertypes]), max([len(corpora[ct].results) for ct in cancertypes])],[20,100])
+ax.scatter(np.where(docoverlap==1, 0, docoverlap).max(axis=1)[[ct in generics for ct in cancertypes]], gscores, s=[sizemap(len(corpora[ct].results)) for ct in generics], marker='o', label='generic')
+ax.scatter(np.where(docoverlap==1, 0, docoverlap).max(axis=1)[[ct in customs for ct in cancertypes]], cscores, s=[sizemap(len(corpora[ct].results)) for ct in customs], marker='^', label='custom')
+ax.legend()
+ax.set_title('Overlap and score')
+ax.set_ylabel('NN score')
+ax.set_xlabel('Maximum overlap with other corpus (%)')
