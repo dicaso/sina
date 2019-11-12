edfe78a397d8eaac97040cae0667af973296cb45
Christophe Van Neste <christophe.vanneste@kaust.edu.sa>
2019-11-12 10:23:03 +0300

    read csv index col

--- sina/paperwork/pancancer.py
+++ sina/paperwork/pancancer.py
@@ -192,11 +192,14 @@ if __name__ == '__main__':
             'testlen': [len(corpora[ct].results_test) for ct in cancertypes]
             }, index=cancertypes
         )
-        corpora_sizes.to_csv(os.path.join(saveloc,'corpora_sizes.csv'), index_col=0)
+        corpora_sizes.to_csv(os.path.join(saveloc,'corpora_sizes.csv'))
         logmemory()
     else:
         corpora = shelve.open(os.path.join(saveloc, 'corpora.shlv'))
-        corpora_sizes = pd.read_csv(os.path.join(saveloc,'corpora_sizes.csv'))
+        corpora_sizes = pd.read_csv(
+            os.path.join(saveloc,'corpora_sizes.csv'),
+            index_col=0
+        )
         if mainprocess:
             # load back in memory
             logging.info('Reloading corpora in main process')
